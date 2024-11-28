fe5a00a3f95252f0d43f509c577a2f9d0c52e79c
sh1baruuu <devillarudolphangelo@gmail.com>
2024-11-28 08:42:14 +0800

    updated

--- src/components/AllForms/Axios.js
+++ src/components/AllForms/Axios.js
@@ -1,17 +1,23 @@
 import axios from "axios";
 
-// Example of setting up Axios instance with token
-const token = localStorage.getItem('token'); // Get your token from localStorage or context
-
 const AxiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://backend-knov.onrender.com/api',
     timeout: 5000,
     headers: {
         "Content-Type": "application/json",
         accept: "application/json",
-        Authorization: `Token ${token}` // Add the token here
     }
 });
+
+
+AxiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+        config.headers.Authorization = `Token ${token}`;
+    }
+    return config;
+});
+
 
 
 export default AxiosInstance;
